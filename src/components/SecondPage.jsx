e46101bc4fb1493069bcbf4d5da65a984ba3c973
Adil-km <adilkmparappur@gmail.com>
2025-01-13 19:40:53 +0530

    update SecondPage.jsx

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -6,11 +6,7 @@ export const SecondPage = () =>{
     <div className='page-second' id='about'>
       <div className="page-second-content">
           <span>About me👋</span>
-          <p>Hi, I 'm Adil, a BVoc Software Development student with a curious and creative mind. I’m passionate about web development 💻 and love diving into new technologies to craft simple yet effective solutions 🔍.
-          
-          When I’ m not coding, you’ ll find me learning new things📚, drawing🎨, or brainstorming innovative ideas💡.My journey is fueled by a constant drive to grow my skills, explore possibilities, and use technology to make a meaningful impact.
-          
-          I believe in continuous learning and embracing challenges to create tools that truly matter🌟.</p>
+          <p>Hi, I 'm Adil, a BVoc Software Development student with a curious and creative mind. I’m passionate about web development 💻 and love diving into new technologies to craft simple yet effective solutions 🔍<br/>When I’ m not coding, you’ ll find me learning new things📚, drawing🎨, or brainstorming innovative ideas💡.My journey is fueled by a constant drive to grow my skills, explore possibilities, and use technology to make a meaningful impact.<br/>I believe in continuous learning and embracing challenges to create tools that truly matter🌟.</p>
       </div>
       </div>
     </>
