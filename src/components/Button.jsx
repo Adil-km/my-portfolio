d39d0aaa404c8c1e090049970633de69abaf77c9
Adil-km <adilkmparappur@gmail.com>
2025-01-13 07:56:31 +0530

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import '../App.css'
 
-export const Button = ({text,glow=true}) =>{
+export const Button = ({text, glow=true, link}) =>{
   return (
   <>
   <button 
   //if doesn't want the glow effect 
-  className={glow ? "btn-glow" : "btn"}>
+  className={glow ? "btn-glow" : "btn"}
+  onClick={()=> (link? (window.location.href= link): null)}>
   {text}
   </button>
   </>);
--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -12,12 +12,13 @@ export const FirstPage = () =>{
               <span>Hello, welcome</span>
               <span className="profile-name">I'm Name</span>
               <span className="page-first-discription">student at farook college</span>
-              <Button className="btn" text="Let's connect"/>
+              <Button className="btn" link="https://www.linkedin.com/in/adil-km" text="Let's connect"/>
           </div>
 
           <img src={profileSrc} className="page-first-img" alt="profile pic"/>
         </div>
       </div>
+
     </>
     );
 }
