007856ddc59481992d685c0994bfcf72bd642d89
Adil-km <adilkmparappur@gmail.com>
2025-01-13 07:28:52 +0530

    update

---
+++ src/components/FirstPage.jsx
@@ -0,0 +1,12 @@
+import { Button } from './Button.jsx';
+
+import '../App.css';
+
+export const FirstPage = () =>{
+  const profileSrc= '/profile.png';
+  return (
+    <>
+    <h1>🙂hil</h1>
+    </>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import {FirstPage} from './FirstPage.jsx';
+
+import '../App.css';
+
+export const Main = ()=>{
+  
+  return (
+    <>
+    <div className="main">
+    <FirstPage/>
+    </div>
+    </>
+  );
+}
