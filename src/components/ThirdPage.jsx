129c97beec10817f5902bda4cbb5c063d2faa2be
Adil-km <adilkmparappur@gmail.com>
2025-01-13 16:01:22 +0530

    update ThirdPage.jsx

--- src/components/ThirdPage.jsx
+++ src/components/ThirdPage.jsx
@@ -3,7 +3,16 @@ import "../App.css"
 export const ThirdPage = () =>{
     return (
         <>
-        <h1>hjuhju</h1>
+          <div className="page-third">
+            <div className="page-third-content">
+              <span>My projects</span>
+              <div className="project-div">
+                <span>project name</span>
+                <span>Technologies used</span>
+                <span>project link</span>
+              </div>
+            </div>
+          </div>
         </>
     )
 }
