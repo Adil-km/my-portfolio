6ef8a9e47d1a5b5ab2f40e22e40f5df0500080c8
Adil-km <adilkmparappur@gmail.com>
2025-01-14 12:18:00 +0530

    update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import '../App.css';
 import { FirstPage } from './FirstPage.jsx';
 import { SecondPage } from './SecondPage.jsx'
 import { ThirdPage } from './ThirdPage.jsx';
-import { FourthPage } from './ThirdPage.jsx';
+//import { FourthPage } from './ThirdPage.jsx';
 export const Main = ()=>{
   
   return (
