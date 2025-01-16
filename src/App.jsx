1250d9ffc6025faf909665bcb1236c91b930c986
adilkm123 <adilkmschool@gmail.com>
2025-01-16 13:56:10 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 
 import {NavBar} from './components/NavBar.jsx';
-import {MainPage} from './components/Main.jsx'
-import { Contact } from './components/Contact.jsx';
+import {MainPage} from './components/MainPage.jsx'
+// import { Contact } from './components/Contact.jsx';
 
 function App() {
 
   return (
     <>
-      {/* <NavBar/>
-      <MainPage/> */}
-      <Contact/>
- 
+      <NavBar/>
+      <MainPage/> 
+  
     </>
   )
 }
