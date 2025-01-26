75b7b675e3a81007c5fb0d862e3c82fb42233a08
Adil-km <adilkmparappur@gmail.com>
2025-01-26 19:34:16 +0530

    update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,50 @@
-import { Button } from "./Button"
-import '../App.css'
+import { Button } from "./Button";
+import '../App.css';
 
+import {useState} from 'react';
 
 export const Contact= () =>{
-    const show = (event) =>{
-      const formData = new FormData(event.target);
-
-      const name = formData.get("name");
-      const email = formData.get("email");
-      const phone = formData.get("tel");
-      const message = formData.get("text");
-      alert(name);
-      }
+    const [formData, setFormData] = useState({
+      name : '',
+      email : '',
+      message : ''
+    });
+    const formChange = (e) => {
+      const {name,value} =  e.target;
+      setFormData({
+        ...formData,
+        [name]:value
+      });
+    }
+    
+    const formSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+      setFormData({
+        name : '',
+        email : '',
+        message : ''
+      })
+    };
+  
   
     return (
       <>
       <div className="form-container">
         <h1>Contact me</h1>
         
-        <form onSubmit={show}>
+        <form onSubmit={formSubmit}>
         
-          <input type="text" name="name" placeholder="Enter your Name"/>
-          <input type="email" name="email" placeholder="Enter your Email"/>
-          <input type="text" name="message" placeholder="Enter your Message"/>
-          <div class="formBtnDiv">
+          <input onChange={formChange} value={formData.name} type="text" name="name" className="formInput"  placeholder="Enter your Name" required/>
+          
+          <input onChange={formChange} value={formData.email}  type="email" name="email" className="formInput"  placeholder="Enter your Email" required/>
+          
+          <textarea onChange={formChange} value={formData.message} name="message"className="formInput" placeholder="Enter your Message" required/>
+          
+          <div className="formBtnDiv">
               <Button text="submit" type="submit"/>
           </div>
+          
          </form>
         </div>
       </>
