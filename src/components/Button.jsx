c6808ab3dd5f706b0a0e6424391044df7fc2d69c
Adil-km <adilkmparappur@gmail.com>
2025-02-22 15:00:25 +0530

    hi

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,11 +13,10 @@ export const Button = ({text, glow=true, link}) =>{
     //if current page is in home
     //for smooth scrolling, I used button instead of Link
   <button className={glow ? "btn-glow" : "btn"} onClick={()=>{window.location.href=link}}>{text}</button>
-  console.log("used buttton");
+  
 ):(
 
-	console.log("used link");
-	console.log(import.meta.env.BASE_URL);
+	
 	
     <Link 
     to={link}
