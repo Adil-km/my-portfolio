97fedd0be58bb4cafba80a9ecef248b18893bfba
Adil-km <adilkmparappur@gmail.com>
2025-02-23 11:45:42 +0530

    edit 6 vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-
-import {NavBar} from './components/NavBar.jsx';
-import {MainPage} from './components/MainPage.jsx'
+import { NavBar } from './components/NavBar.jsx';
+import { MainPage } from './components/MainPage.jsx';
 import { Contact } from './components/Contact.jsx';
-import {Footer} from './components/Footer.jsx';
-import { Route, Routes, useLocation } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
-
   const location = useLocation();
-
+  
   return (
     <>
-        {/* to hide the navbar when in contact page */}
-        
+      {/* Hide NavBar when on the Contact page */}
       {location.pathname !== "/contact" && <NavBar />}
-    <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-    {/* <Footer /> */}
-    
+      
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // Export the Vite configuration
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the base path is correctly set for Vercel
 });
