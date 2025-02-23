3e49d8923a1f95e5606be5075cf30f1aa6a6c5f5
Adil-km <adilkmparappur@gmail.com>
2025-02-23 11:54:39 +0530

    web analytics of vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { NavBar } from './components/NavBar.jsx';
 import { MainPage } from './components/MainPage.jsx';
 import { Contact } from './components/Contact.jsx';
 import { Route, Routes, useLocation } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 function App() {
   const location = useLocation();
   
   return (
     <>
+	<Analytics />
       {/* Hide NavBar when on the Contact page */}
       {location.pathname !== "/contact" && <NavBar />}
       
