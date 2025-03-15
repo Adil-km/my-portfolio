c2cf25722a34351d59f56b6575e98489bdb19445
Adil-km <adilkmparappur@gmail.com>
2025-03-15 09:47:11 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/contact" element={<Contact />} />
         
-       
+        <Route path="/error" element={<ErrorPage/>}/>
       </Routes>
     </>
   );
