8ee0ded29fc93f154e251c18fc970cc35d51270b
Adil-km <adilkmparappur@gmail.com>
2025-03-15 09:33:46 +0530

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { NavBar } from './components/NavBar.jsx';
 import { MainPage } from './components/MainPage.jsx';
 import { Contact } from './components/Contact.jsx';
+import { Err } from './components/ErrorPage.jsx';
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Analytics } from '@vercel/analytics/react';
 
@@ -17,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/contact" element={<Contact />} />
+        
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </>
   );
---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { Button } from './Button.jsx';
+
+import '../App.css';
+
+export const ErrorPage= () =>{
+  const profileSrc= '/profile.png';
+  return (
+    <>
+    <div className="error-container">
+      <p className="error-main">404</p>
+      <p className="error-message">Oops, This Page Not Found!</p>
+      <Button link='/' text="Go home"/>
+    </div>
+
+    </>
+    );
+}
