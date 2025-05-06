6585e58eff771c6e2aa69596ab258c7e5458d690
adil-km <adilkmparappur@gmail.com>
2025-05-06 11:17:39 +0530

    Integrated email js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,65 +1,130 @@
 import { Button } from "./Button";
 import '../App.css';
 
-import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 
-export const Contact= () =>{
+
+
+export const Contact= ()  => {
   
-    const [formData, setFormData] = useState({
-      name : '',
-      email : '',
-      message : ''
-    });
-    const formChange = (e) => {
-      const {name,value} =  e.target;
-      setFormData({
-        ...formData,
-        [name]:value
-      });
-    }
-    
-    const navigate = useNavigate();
-    
-    const formSubmit = (e) => {
-      if(formData.name!==""&&formData.email!==""){
-      e.preventDefault();
-      console.log(formData);
-      setFormData({
-        name : '',
-        email : '',
-        message : ''
-      })
+  const SERVICE_ID ='service_ey6de8j';
+  const TEMPLATE_ID = 'template_pddexvl';
+  const PUBLIC_KEY = 'plIlCCVIcBu9th20k';
   
-     navigate('/success');
-      }
+  const form = useRef();
+  const navigate = useNavigate();
+  
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
+      publicKey: PUBLIC_KEY,
+    })
+    .then(
+      () => {
+        console.log('SUCCESS!');
+        navigate('/success');
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+      },
+      );
     };
-  
-  
+    
     return (
+      
       <>
-      <div className="form-container">
+            <div className="form-container">
         
         <div className="form-back-btn">
         <Button className="btn" text="Go back" link="/"/>
         </div>
         <h1>Contact me</h1>
         
-        <form onSubmit={formSubmit}>
+        <form ref={form} onSubmit={sendEmail}>
         
-          <input onChange={formChange} value={formData.name} type="text" name="name" className="formInput"  placeholder="Enter your Name" required/>
+          <input type="text" name="name" className="formInput"  placeholder="Enter your Name" required/>
           
-          <input onChange={formChange} value={formData.email}  type="email" name="email" className="formInput"  placeholder="Enter your Email" required/>
+          <input type="email" name="email" className="formInput"  placeholder="Enter your Email" required/>
           
-          <textarea rows={1} onChange={formChange} value={formData.message} name="message" className="formInput" placeholder="Enter your Message" required/>
+          <textarea rows={1} name="message" className="formInput" placeholder="Enter your Message" required/>
           
           <div className="formBtnDiv">
-              <button type="submit" className="btn-glow" onClick={(e)=>{formSubmit(e)}}>Submit</button>
+              <input type="submit" className="btn-glow" value="send"/>
           </div>
           
          </form>
         </div>
-      </>
-    );
-  }
+
+        </>
+
+);
+};
+
+
+// old code
+
+// import {useState} from 'react';
+// export const Contact= () =>{
+  
+  //     const [formData, setFormData] = useState({
+    //       name : '',
+    //       email : '',
+    //       message : ''
+    //     });
+//     const formChange = (e) => {
+//       const {name,value} =  e.target;
+//       setFormData({
+//         ...formData,
+//         [name]:value
+//       });
+//     }
+    
+//     const navigate = useNavigate();
+    
+//     const formSubmit = (e) => {
+//       if(formData.name!==""&&formData.email!==""){
+//       e.preventDefault();
+//       console.log(formData);
+//       setFormData({
+//         name : '',
+//         email : '',
+//         message : ''
+//       })
+  
+//      navigate('/success');
+//       }
+//     };
+  
+  
+//     return (
+//       <>
+//       <div className="form-container">
+        
+//         <div className="form-back-btn">
+//         <Button className="btn" text="Go back" link="/"/>
+//         </div>
+//         <h1>Contact me</h1>
+        
+//         <form onSubmit={formSubmit}>
+        
+//           <input onChange={formChange} value={formData.name} type="text" name="name" className="formInput"  placeholder="Enter your Name" required/>
+          
+//           <input onChange={formChange} value={formData.email}  type="email" name="email" className="formInput"  placeholder="Enter your Email" required/>
+          
+//           <textarea rows={1} onChange={formChange} value={formData.message} name="message" className="formInput" placeholder="Enter your Message" required/>
+          
+//           <div className="formBtnDiv">
+//               <button type="submit" className="btn-glow" onClick={(e)=>{formSubmit(e)}}>Submit</button>
+//           </div>
+          
+//          </form>
+//         </div>
+//       </>
+//     );
+//   }
   
