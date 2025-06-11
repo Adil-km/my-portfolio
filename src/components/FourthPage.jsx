ca0e86c8a276f82eecf665e68647d36fc5ea8587
adil-km <adilkmparappur@gmail.com>
2025-06-11 18:58:26 +0530

    some style effects

--- src/components/FourthPage.jsx
+++ src/components/FourthPage.jsx
@@ -8,26 +8,29 @@ export const FourthPage = () => {
 
   const projectList = project.slice().sort((a, b) => b.id - a.id).map(project =>
 
+    
     <div className="card-items" key={project.id}>
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <ul>{project.features.map((features,index) => (
-    <li key={index}>{features}</li>
-         ))}
-          </ul>
-      <div className="bubble-icons">
-        {project.tech.map((tech, index) =>(
-          <span key={index}>{tech}</span>
+      <div>
+
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+        <ul>{project.features.map((features,index) => (
+            <li key={index}>{features}</li>
           ))}
+        </ul>
+        <div className="bubble-icons">
+          {project.tech.map((tech, index) =>(
+            <span key={index}>{tech}</span>
+            ))}
         </div>
+
+      </div>
         
-        <div>
-          
-            <Button link={project.demo} text="view demo"/>
-            <Button link={project.code} text="view code"/>
-      
+      <div>
+        <Button link={project.demo} text="view demo"/>
+        <Button link={project.code} text="view code"/>
       </div>
-      </div>
+    </div>
 
   );
 
