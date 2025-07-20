63ae1e78d14de463012e9d0741e57c5642a7dfa2
adil-km <adilkmparappur@gmail.com>
2025-07-20 23:16:21 +0530

    meta tags fix 2

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,8 +7,6 @@ import {Footer} from './Footer.jsx';
 
 export const MainPage = ()=>{
   
-  const portfolioDescription = "Welcome to my portfolio website. Explore my web design and development projects by Adil.";
-
   return (
     <>
 
