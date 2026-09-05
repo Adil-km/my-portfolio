768ff1db23ccd5bb1e7f520a07051d97149e76d2
Adil-km <adilkmparappur@gmail.com>
2026-09-05 14:27:22 +0530

    feat: added resume button on HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import { Button } from './Button.jsx';
 
 import '../App.css';
+import myResumePDF from "/resource/ADIL-KM_Resume.pdf"
 
 export const HeroSection = () =>{
   const profileSrc= '/profile.png';
@@ -13,9 +14,14 @@ export const HeroSection = () =>{
               <span>Hello, welcome</span>
               <span className="profile-name">I&apos;m Adil</span>
               <span className="hero-section-description">Software Developer</span>
-              <span>
+              <span className='button-wrapper'>
+
+                <span className='button-wrapper resumeBtn'>
+                  <Button link={myResumePDF} glow={false} text="Resume" target='_blank' />
+                </span>
+                
                 <span className='magnet button-wrapper'>
-                  <a className='hit-area' href={myLinkedin}></a>
+                  <a className='hit-area' target='_blank' href={myLinkedin}></a>
                   <Button link={myLinkedin} text="Let's connect" target='_blank' />
                 </span>
               
